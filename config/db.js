1afce98b18938409ce4b0c2958758fa084655246
Nasar <NasarKamish@gmail.com>
2021-11-29 09:48:19 +0200

    Updated file

---
+++ config/db.js
@@ -0,0 +1,8 @@
+module.exports = {
+  DATA: "h4ldb",
+  USER: "node_user",
+  PASS: "node_password",
+  PORT: 5432,
+  HOST: "localhost",
+  DEFAULTPW: "1234567890",
+};
---
+++ config/mailer.js
@@ -0,0 +1,6 @@
+module.exports = {
+  USER: "i37xdoqalf5xrv43@ethereal.email",
+  PASS: "xbSu7r1h6DeMjbPzBR",
+  HOST: "smtp.ethereal.email",
+  PORT: 587,
+};
